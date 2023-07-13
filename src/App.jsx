5793a6f591719c1617d13962a6d9d37b559b37e2
federicocoletto <federico.coletto@fce.uncu.edu.ar>
2023-07-13 12:05:03 -0300

    [limpieza]

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className="app">
+			<header className="app__header section">
+				<p className="header__name">FEDERICO</p>
+				<p className="header__lastname">COLETTO</p>
+				<p className="header__intro">Hola, soy un programador junior</p>
+			</header>
+		</div>
+	)
+}
+
+export default App
